3006227ffeabd30aa584518787611d3e731f26e5
ayeshamukhtar007 <you@example.com>
2022-07-17 18:14:34 +0500

    connect DB

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const { response } = require("express");
 dotenv.config();
 
 mongoose
-  .connect(process.env.MONGO_URL)
+  .connect("mongodb+srv://FYP:fyp@cluster0.38nw6.mongodb.net/?retryWrites=true&w=majority")
   .then(() => console.log("DB Connection Successfull!"))
   .catch((err) => {
     console.log(err);
