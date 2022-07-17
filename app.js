2bce4c80c15fb817f67518a0a2cfb16ebdfd609a
ayeshamukhtar007 <you@example.com>
2022-07-17 16:13:42 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const authRoute = require("./routes/auth");
+const cameraRoute = require("./routes/camera");
+const serviceRoute = require("./routes/service");
+const adminRoute = require("./routes/admin");
+const userRoute = require("./routes/user");
+const alertRoute = require("./routes/alert");
+
+const cors = require("cors");
+const { response } = require("express");
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("DB Connection Successfull!"))
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.use(cors());
+app.use(express.json());
+app.get("/", (req,res)=>{
+  let response="hello";
+  res.status(200).json(response);
+});
+app.use("/api/auth", authRoute);
+// app.use("/api/camera", cameraRoute);
+// app.use("/api/admin", adminRoute);
+// app.use("/api/user", userRoute);
+// app.use("/api/alert", alertRoute);
+// app.use("/api/service", serviceRoute);
+// app.listen(process.env.PORT || 5000, () => {
+//     console.log("Backend server is running!");
+//   });
+module.exports = app;
